4908f8f2b3a58d83cd03f09eef3e5c191c6ce79a
hisa11 <132685722+hisa11@users.noreply.github.com>
2025-05-25 00:24:08 +0900

    バージョン1.0.4

--- pyfile/UI/window.py
+++ pyfile/UI/window.py
@@ -27,7 +27,7 @@ class InfoDialog(QDialog):
     self.setGeometry(100, 100, 200, 100)
     layout = QVBoxLayout()
 
-    version_label = QLabel("バージョン: 1.0.2")
+    version_label = QLabel("バージョン: 1.0.4")
     layout.addWidget(version_label)
 
     code_button = QPushButton("コード")
